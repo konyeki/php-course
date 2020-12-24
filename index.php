4202b4f91c8662f5ec6beab07150319a8812cfda
konyeki <konye.evans@yahoo.com>
2020-12-24 23:04:23 +0300

    welcome message

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP store</title>
+</head>
+<body>
+	<?php 
+	// Using two forward slashes we create comments in php.
+    // Within the php tag, we can insert values into our HTML.
+    echo "<h1>Welcome!</h1>";
+	 ?>
+
+</body>
+</html>
